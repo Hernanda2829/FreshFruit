39118b35f8230efe20943a4dfad34972732e0f97
hernanda giri <hernanda.pramudita@students.amikom.ac.id>
2020-11-19 18:08:44 +0700

    freshFruit

---
+++ FreshFruit/Controller/BucketController.cs
@@ -0,0 +1,50 @@
+﻿using FreshFruit.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FreshFruit.controller
+{
+    class BucketController
+    {
+        private Bucket bucket;
+        private BucketEventListener eventListener;
+
+        public BucketController(Bucket bucket, BucketEventListener eventListener)
+        {
+            this.bucket = bucket;
+            this.eventListener = eventListener;
+        }
+        public void addFruit(Fruit fruit)
+        {
+            if (bucketIsOverload())
+            {
+                eventListener.onFailed("Ops, keranjang penuh");
+            }
+            else
+            {
+                this.bucket.insert(fruit);
+                eventListener.onSucceed("Yey, berhasil ditambahkan");
+            }
+        }
+        public bool bucketIsOverload()
+        {
+            return bucket.countItems() >= bucket.getCapacity();
+        }
+
+        public void removeFruit(Fruit fruit)
+        {
+            for (int itemPosition = 0; itemPosition < bucket.countItems(); itemPosition++)
+            {
+                if (bucket.findAll().ElementAt(itemPosition).getName() == fruit.getName())
+                {
+                    bucket.remove(itemPosition);
+                    eventListener.onSucceed("Yey, berhasil dihapus");
+                }
+            }
+        }
+        public List<Fruit> findAll()
+        {
+            return this.bucket.findAll();
+        }
+    }
+}
